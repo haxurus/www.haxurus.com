374ddfbb4f92261a73572e9b5c5f4a41b262d53b
Haxurus <72351758+haxurus@users.noreply.github.com>
2026-07-02 18:53:16 +0200

    Add Nagisa anime choice patch

---
+++ scripts/bemyegirl-nagisa-patch.js
@@ -0,0 +1,50 @@
+const susAudioPatch = new Audio("audio/sus.mp3");
+susAudioPatch.preload = "auto";
+
+function addNagisaChoicePatch() {
+  const question = questions.find((item) => item.id === "principessa_disney");
+  if (!question) return;
+  if (question.answers.some((answer) => answer.label === "Nagisa Shiota")) return;
+
+  question.answers.push({
+    label: "Nagisa Shiota",
+    image: "/img/bemyegirl/nagisashiota.png",
+    value: 0,
+    action: "susSound"
+  });
+}
+
+const nagisaBaseHandleSingleAnswer = handleSingleAnswer;
+handleSingleAnswer = function(question, answerIndex) {
+  const answer = question.answers[answerIndex];
+
+  if (answer?.action !== "susSound") {
+    nagisaBaseHandleSingleAnswer(question, answerIndex);
+    return;
+  }
+
+  state.answers[question.id] = {
+    answerIndex,
+    value: answer.value ?? null,
+    action: answer.action ?? null
+  };
+
+  playAudio(susAudioPatch);
+  renderQuestion();
+
+  window.setTimeout(() => {
+    if (state.currentIndex < questions.length - 1) {
+      state.currentIndex += 1;
+      renderQuestion();
+      return;
+    }
+
+    renderResult();
+  }, 850);
+};
+
+addNagisaChoicePatch();
+
+if (!state.restartMode && typeof renderQuestion === "function") {
+  renderQuestion();
+}
